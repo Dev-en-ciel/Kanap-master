977c5fa2ec5c058ceae6e6a8661564c02412910f
Devenciel <leplat.franck@numericable.fr>
2022-01-10 15:35:01 +0100

    ajout fichier + mise a jour

--- front/js/script.js
+++ front/js/script.js
@@ -1,9 +1,10 @@
 // récuperation des ressources de L'API
 fetch("http://localhost:3000/api/products")
     .then(res => res.json())
+
 // ajouts des Objets sur la page d'accueil 
     .then((productsItems) => {
-         productsItems.forEach((product) => {       // Utlisation d'une boucle for each pour itérer les données du tableau
+         productsItems.forEach((product) => {       // Utlisation d'une boucle for each pour itérer(répeter la tâche jusqu'au dernier produit) les données du tableau
             document.querySelector("#items").innerHTML += ` 
                 <a href="./product.html?id=${product._id}">
                 <article>
@@ -14,5 +15,6 @@ fetch("http://localhost:3000/api/products")
                 </a>`;
         });
     })
-    // retour message au cas ou la récuperation de l'api a échouée
-    .catch (error => ("Erreur lors de la récuperation de l api"))
+    // retour message au cas ou la récuperation des ressources a échouée
+    .catch (_error => alert("Erreur lors de la connexion au server")) 
+    
