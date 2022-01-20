9f4a96cf442a0eb60a3d2312d3b985cff54a147b
Devenciel <leplat.franck@numericable.fr>
2022-01-20 16:53:19 +0100

    test

--- front/js/product.js
+++ front/js/product.js
@@ -1,32 +1,82 @@
-// Récupération de l'adresse de la page index.html avec window.location.href
-const recupUrl = window.location.href;
-console.log(recupUrl)
-// Récupération de l'ID avec urlSearchParams
-const recupId = window.location.search;
-const urlSearchParams = new URLSearchParams(recupId);
-const id = urlSearchParams.get('id')
-console.log(id);
-// récuperation des ressources de l'api avec l'id du produit
-fetch('http://localhost:3000/api/products/' + id)
-  .then((res) => {
+window.onload = () => {
 
-  const response = res.json();
-  response.then(product =>{
+  // Récupération de l'adresse de la page index.html avec window.location.href
+  const recupUrl = window.location.href;
 
-        let addToCart = document.getElementById("addToCart");
-        let colors = document.getElementById("colors");
-        let img = document.createElement("img");
-        let quantity = document.querySelector("#quantity");
-        let price = document.getElementById("price").innerHTML = product.price;
-        let name = document.getElementById("title").innerHTML = product.name;
-        
-        // Afficher les élèments sur la page produits
-        document.querySelector(".item__img").appendChild(img);
-        document.querySelector("#colors").insertAdjacentHTML("beforeend", product.colors.map(color => `<option id= "valueColor" value="${color}">${color}</option>`));
-        document.getElementById("description").innerHTML = product.description;
-        img.src = product.imageUrl;
-        img.alt= product.altTxt;
-        let image = img.src ;
-        let imageAlt= img.alt;
-});
+  // Récupération de l'ID avec urlSearchParams
+  const recupId = window.location.search;
+  const urlSearchParams = new URLSearchParams(recupId);
+  const id = urlSearchParams.get('id')
+
+  // Récupération des ressources de l'api avec l'id du produit
+  const productsId = fetch('http://localhost:3000/api/products/' + id)
+  productsId.then(async (responseData) => {
+
+    const product = await responseData.json();
+
+    // Affichage des détails du produit sélectionné
+    document.getElementById("addToCart");
+    document.getElementById("price").innerHTML = product.price;
+    document.getElementById("title").innerHTML = product.name;
+    document.getElementById("description").innerHTML = product.description;
+    let img = document.createElement("img");
+    img.src = product.imageUrl;
+    img.alt = product.altTxt;
+    document.querySelector(".item__img").appendChild(img);
+
+    // Gestion des couleurs. je creer une boucle pour l'affichage des couleurs 
+    for (color in product.colors) {
+      colors.options[colors.options.length] = new Option(product.colors[color]);
+    }
+    // panier.
+    // //Je créer des constantes pour le choix de l'utilisateur
+    let productColors = document.getElementById("color");
+    let imageURL = "imageURL";
+    let altTxt = "altTxt";
+    let quantities = Number(quantity.value);
+
+    console.log(color)
+
+    // évenement pour le bouton ajouter au panier
+   let addToCart = document.getElementById('addToCart').addEventListener("click", (event) => {
+      event.preventDefault();
+      // Valeurs du produit récupérées à stockées dans un tabeau.
+      const productInfo = {
+        id,
+        name,
+        imageURL,
+        altTxt,
+        colors,
+        quantities,
+      };
+
+      // tableau pour récuperer les données 
+      let saveProducts = [];
+
+      // condition pour vérifier que le tableau n'a pas de produit enregistrer
+      if (localStorage.getItem("product")) {
+        // si le tableau contient des donnés, elle sont transferées dans le tableau
+        saveProducts = JSON.parse(localStorage.getItem("product"));
+
+        // vérification qu'il n y as pas de produit avec le meme id et la meme couleur
+        let kanap = saveProducts.findIndex((object => object.id === productInfo.id && productInfo.color === object.color));
+
+        // si il y as déjà un produit identique alors la quantité est modifiée
+        if (kanap !== 0) {
+          saveProducts[kanap].quantities += quantities;
+        }
+        // si ce n'est pas le cas alors le produit est ajouté au tableau
+        else if (kanap === 0) {
+        }
+        // les produits du tableau du localStorage et sont convertis en chaine de caractères
+        localStorage.setItem("product", JSON.stringify(saveProducts));
+      }
+
+      // sinon si le localStorage est vide, les données son envoyées avec un tableau et les convertis 
+      else {
+        saveProducts.push(productInfo);
+        localStorage.setItem("product", JSON.stringify(saveProducts));
+      }
+    });
   });
+}
--- front/js/script.js
+++ front/js/script.js
@@ -1,20 +1,24 @@
+
+window.onload = () => {
 // récuperation des ressources de L'API
 fetch("http://localhost:3000/api/products")
     .then(res => res.json())
 
-// ajouts des Objets sur la page d'accueil 
+    // ajouts des Objets sur la page d'accueil 
+    // Utlisation d'une boucle for each pour itérer les données du tableau (répeter la tâche jusqu'au dernier produit) 
     .then((productsItems) => {
-         productsItems.forEach((product) => {       // Utlisation d'une boucle for each pour itérer les données du tableau (répeter la tâche jusqu'au dernier produit) 
+        productsItems.forEach((product) => {
             document.querySelector("#items").innerHTML += ` 
                 <a href="./product.html?id=${product._id}">
                 <article>
                     <img src="${product.imageUrl}" alt="${product.altTxt}">
-                    <h3 class="productName">"${product.name}"</h3>
+                    <h3 class="productName">${product.name}</h3>
                     <p class="productDescription">${product.description}</p>
                 </article>
                 </a>`;
         });
     })
     // retour message au cas ou la récuperation des ressources a échouée
-    .catch (_error => alert("Erreur lors de la connexion au server")) 
-    
+    .catch(_error => ("Erreur lors de la connexion au server"))
+    // faire en sorte d'affiche un message d'erreur en pleine page.
+}
