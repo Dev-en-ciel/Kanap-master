ba9f12920b9c14c96fa52dad9d3dc71f57835fe9
Devenciel <leplat.franck@numericable.fr>
2022-01-26 10:55:20 +0100

    modif

--- front/js/product.js
+++ front/js/product.js
@@ -1,15 +1,15 @@
-// récupération de l'id du produit dans l'URL
+// Récupération de l'id du produit dans l'URL
 function recupProductId() {
   let params = new URL(window.location.href).searchParams;
   return params.get('id');
 }
 
-//récuperation des détails du produits sélèctionné 
+//Récuperation des détails du produits sélèctionné 
 function productDetail(productId) {
   fetch("http://localhost:3000/api/products/" + productId)
     .then(res => res.json())
     .then(product => {
-      
+
       let image = document.getElementsByClassName('item__img')
       image[0].innerHTML = `<img src="${product.imageUrl}" alt=${product.altTxt}">`;
       imageURL = product.imageUrl;
@@ -18,88 +18,84 @@ function productDetail(productId) {
       document.getElementById('price').innerHTML = `${product.price}`;
       document.getElementById('description').innerHTML = `${product.description}`;
 
-      // choix des couleurs
-
+// Mise en place du choix des couleurs 
       for (number in product.colors) {
         colors.options[colors.options.length] = new Option(product.colors[number]);
       }
     })
-    .catch((error) => {
-      console.error({ "error": error })
-    })
+      if (colors === ''){
+        window.alert('veuillez sélectionner au moins une couleur')
+      }
 }
- 
 
-
-  //  ajout au panier
-  function addCart() {
+//ajoutez les produits au panier
+function addCart() {
   // mise en place des variables
   const choiceQuantity = document.getElementById('quantity');
   const choiceColors = document.getElementById('colors');
   const addToCart = document.getElementById('addToCart');
   let imageURL = "";
-  let altTxt = "";
-    addToCart.addEventListener('click', (event) => {
-      event.preventDefault();
+     addToCart.addEventListener('click', (event) => {
+    event.preventDefault();
 
-      const details = {
-        id: productId,
-        image: imageURL,
-        name: title.textContent,
-        color: choiceColors.value,
-        quantity: choiceQuantity.value,
+  const details = {
+  id: recupProductId,
+    image: imageURL,
+      name: title.textContent,
+      color: choiceColors.value,
+      quantity: choiceQuantity.value,
 
-      };
+    };
 
-      // je met clés+valeurs dans le localStorage
-      let productInLocalStorage = JSON.parse(localStorage.getItem('product'));
+    // je met clés+valeurs dans le localStorage
+    let productInLocalStorage = JSON.parse(localStorage.getItem('product'));
 
-      //j'ajoute les produits choisis dans le localstorage
-      const addInLocalStorage = () => {
+    //j'ajoute les produits choisis dans le localstorage
+    const addInLocalStorage = () => {
 
-        //je récupère le choix du client et les données du localstorage
-        productInLocalStorage.push(details);
-        localStorage.setItem('product', JSON.stringify(productInLocalStorage));
-      }
+      //je récupère le choix du client et les données du localstorage
+      productInLocalStorage.push(details);
+      localStorage.setItem('product', JSON.stringify(productInLocalStorage));
+    }
 
-      const addConfirm = () => {
-        alert('Votre produit a bien été ajouté');
-      }
-          // alert('veuillez sélectionnez une quantité ou/et une couleur ')
+    const addConfirm = () => {
+      alert('Votre produit a bien été ajouté');
+    }
+    // alert('veuillez sélectionnez une quantité ou/et une couleur ')
 
-      let update = false;
+    let update = false;
 
-      //Verification qu'il n'y a pas les mêmes produits avec une boucle
-      if (productInLocalStorage) {
-        productInLocalStorage.forEach(function (productconfirmation, key) {
-          if (productconfirmation.id === productId && productconfirmation.color === choiceColors.value) {
-            productInLocalStorage[key].quantity = parseInt(productconfirmation.quantity) + parseInt(choiceQuantity.value);
-            localStorage.setItem('product', JSON.stringify(productInLocalStorage));
-            update = true;
-            addConfirm();
-          }
-        });
-
-        if (!update) {
-          addInLocalStorage();
+    //Verification qu'il n'y a pas les mêmes produits avec une boucle
+    if (productInLocalStorage) {
+      productInLocalStorage.forEach(function (productconfirmation, key) {
+        if (productconfirmation.id === recupProductId && productconfirmation.color === choiceColors.value) {
+          productInLocalStorage[key].quantity = parseInt(productconfirmation.quantity) + parseInt(choiceQuantity.value);
+          localStorage.setItem('product', JSON.stringify(productInLocalStorage));
+          update = true;
           addConfirm();
         }
-      }
+      });
 
-      else {
-        productInLocalStorage = [];
+      if (!update) {
         addInLocalStorage();
         addConfirm();
       }
-    });
-  }
+    }
+
+    else {
+      productInLocalStorage = [];
+      addInLocalStorage();
+      addConfirm();
+    }
+  });
+}
 
 
 
 window.onload = () => {
   recupProductId();
-  let bidon = recupProductId();_
-   // fonction qui répete la fonction çi-dessus pour ne pas ré-écrire le code
-  productDetail(bidon);
+  let id = recupProductId();
+  // fonction qui répete la fonction çi-dessus pour ne pas ré-écrire le code
+  productDetail(id);
   addCart();
 }
